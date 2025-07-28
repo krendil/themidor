41143f8781a922b75f3750024d5c653ab0417946
David Osborne <david.osborne@protonmail.com>
2025-07-28 19:30:57 +1000

    Make smarter guesses when adding a new hue or shade.

---
+++ src/library/palette-tools.test.ts
@@ -0,0 +1,68 @@
+import { expect, test, describe } from 'vitest'
+
+import { midpointOfBiggestGap } from './palette-tools'
+
+describe('midpointOfBiggestGap', () => {
+
+  test('should find midpoint in the middle of sequence', () => {
+    // Arrange
+    const values = [ 1, 5, 7, 9 ];
+    const expected = 3;
+    // Act
+    const actual = midpointOfBiggestGap(values, 10, false);
+    // Assert
+    expect(actual).toEqual(expected);
+  });
+
+  test('should find midpoint at the end of sequence', () => {
+    // Arrange
+    const values = [ 1, 3, 5, 7 ];
+    const expected = 8.5;
+    // Act
+    const actual = midpointOfBiggestGap(values, 10, false);
+    // Assert
+    expect(actual).toEqual(expected);
+  });
+
+  test('should find midpoint at the beginning of sequence', () => {
+    // Arrange
+    const values = [ 3, 5, 7, 9 ];
+    const expected = 1.5;
+    // Act
+    const actual = midpointOfBiggestGap(values, 10, false);
+    // Assert
+    expect(actual).toEqual(expected);
+  });
+
+
+  test('should find midpoint in the middle of circle', () => {
+    // Arrange
+    const values = [ 1, 5, 7, 9 ];
+    const expected = 3;
+    // Act
+    const actual = midpointOfBiggestGap(values, 10, true);
+    // Assert
+    expect(actual).toEqual(expected);
+  });
+
+  test('should find midpoint at the end of circle', () => {
+    // Arrange
+    const values = [ 1, 3, 5, 7 ];
+    const expected = 9;
+    // Act
+    const actual = midpointOfBiggestGap(values, 10, true);
+    // Assert
+    expect(actual).toEqual(expected);
+  });
+
+  test('should find midpoint at the beginning of circle', () => {
+    // Arrange
+    const values = [ 3, 5, 7, 9 ];
+    const expected = 1;
+    // Act
+    const actual = midpointOfBiggestGap(values, 10, true);
+    // Assert
+    expect(actual).toEqual(expected);
+  });
+
+})
--- src/library/palette-tools.ts
+++ src/library/palette-tools.ts
@@ -24,14 +24,68 @@ export function guessNewColour(palette: Palette, hue: number, shade: number): Co
     .value();
 
   if( isNaN(l) ) {
-    l = Math.random();
+    // Lightness fallback: look for the biggest gap in shades of the hue
+    let ls = chain(palette.colours[hue])
+      .filter( pm => pm !== null )
+      .map( pm => oklch(pm.colour).l )
+      .sort((a, b) => a - b)
+      .value();
+
+    l = midpointOfBiggestGap(ls, 1, false);
   }
   if( isNaN(c) ) {
-    // Min and max chroma varies depending on hue and lightness. True max is ~0.4, but 0.3 is practical max
-    c = Math.random() * 0.3;
+    // Chroma fallback: average chroma for hues of the shade
+    c = chain(palette.colours)
+      .map( row => row[shade] )
+      .filter( pm => pm !== null )
+      .map( pm => oklch(pm.colour).c )
+      .mean()
+      .value();
   }
   if( isNaN(h) ) {
-    h = Math.random() * 360;
+    // Hue fallback: look for the biggest gap in hues of the shade
+    const hs = chain(palette.colours)
+      .map( row => row[shade] )
+      .filter( pm => pm !== null )
+      .map( pm => oklch(pm.colour).h ?? 0 )
+      .sort((a, b) => a - b)
+      .value();
+    h = midpointOfBiggestGap(hs, 360, true);
   }
   return {mode: 'oklch', l, c, h };
+}
+
+export function midpointOfBiggestGap(values: number[], maxValue: number, circular = false): number {
+  let first = 0;
+  if(circular) {
+    first = values[values.length-1] - maxValue;
+  }
+  let previous = first;
+  let next = first;
+  let biggestGap = 0;
+  let gapIndex = 0;
+
+  for(let i = 0; i < values.length; i++) {
+    const gap = values[i] - previous;
+    if(gap > biggestGap) {
+      biggestGap = gap;
+      gapIndex = i;
+    }
+    previous = values[i];
+  }
+  const gap = maxValue - previous;
+  if(gap > biggestGap) {
+    next = maxValue;
+    // previous is already the correct value
+  } else {
+    next = values[gapIndex];
+    previous = gapIndex == 0 ? first : values[gapIndex - 1];
+  }
+
+  const midpoint = (next + previous) / 2;
+  if(midpoint < 0 && circular) {
+    return midpoint + maxValue;
+  } else {
+    return midpoint;
+  }
 }
