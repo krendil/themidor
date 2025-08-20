a807652d045d9af86da177ce039f83508a5b7160
David Osborne <david.osborne@protonmail.com>
2025-08-20 10:24:13 +1000

    Fix preview background not applying to scrolled area

--- src/views/TagView.vue
+++ src/views/TagView.vue
@@ -209,8 +209,8 @@ const tagVars = computed<{ [key: string]: string }>(() =>
 }
 
 #preview :deep(> *) {
-  height: 100%;
-  width: 100%;
+  min-height: 100%;
+  min-width: 100%;
   margin: 0;
   padding: 0.5rem;
 }
