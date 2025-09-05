9e95739bd314dc3544f79c32569d129fa0e42ae2
David Osborne <david.osborne@protonmail.com>
2025-09-05 20:00:54 +1000

    Update Base24 exporter to modern non-deprecated format

--- src/components/exporters/Base24.vue
+++ src/components/exporters/Base24.vue
@@ -6,9 +6,11 @@ import Base24Template from './Base24.liquid?raw';
 import BaseExporter from './BaseExporter.vue';
 import { makeLiquidEngine } from '@/library/template-utils';
 import { slugify } from '@/library/text-utils';
+import { oklch } from 'culori';
 
 interface Base24Opts {
-    author: string
+    author: string,
+    description: string
 }
 
 const paletteStore = usePaletteStore();
@@ -18,7 +20,8 @@ const opts: Ref<Base24Opts> = toRef(paletteStore.palette.exportOptions, "Base24"
 
 if(opts.value === undefined) {
     opts.value = {
-        author: "Anonymous"
+        author: "Anonymous",
+        description: "Created with Themidor"
     };
 }
 
@@ -34,22 +37,37 @@ const required = [
 const recommended = [
 ];
 
+const variant = computed(() => {
+    const bg = paletteStore.getColourByTag("b24:00")?.colour;
+    if( !!bg && oklch(bg).l > 0.5) {
+        return "light";
+    } else {
+        return "dark";
+    }
+});
+
 const output = computed(() => {
     return engine.parseAndRenderSync(Base24Template, {
         author: opts.value.author,
-        name: paletteStore.palette.name
+        description: opts.value.description,
+        name: paletteStore.palette.name,
+        slug: slugify(paletteStore.palette.name),
+        variant: variant.value
     });
 });
+
+const filename = computed(() => "base24-" + slugify(paletteStore.palette.name)+".yaml");
 
 </script>
 
 <template>
-    <BaseExporter :output="output" :filename="slugify(paletteStore.palette.name)+'.yaml'" :required :recommended>
+    <BaseExporter :output :filename :required :recommended>
         <div class="tray">
             Base24 themes can be used to generate colour themes for many different programs. See <a href="https://github.com/tinted-theming/home?tab=readme-ov-file#supported-applications">here</a> for more info.
         </div>
         <div class="tray">
             <label>Author: <input type="text" v-model="opts.author"></input></label>
+            <label>Description: <input type="text" v-model="opts.description"></input></label>
         </div>
     </BaseExporter>
 </template>
