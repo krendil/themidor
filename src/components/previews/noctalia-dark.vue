15f9c3c665c42a57858ac0b555f85015e42751b6
David Osborne <david.osborne@protonmail.com>
2026-02-02 13:15:07 +1000

    Add Noctalia previews

---
+++ src/components/previews/noctalia-dark.vue
@@ -0,0 +1,61 @@
+<script setup lang="ts">
+import { usePaletteStore } from '@/stores/palette';
+
+
+const paletteStore = usePaletteStore();
+
+</script>
+
+<template>
+
+<div style="background-color: var(--noct-dark-surface); color: var(--noct-dark-onsurface);">
+
+     <h2 class="align-center">Surface</h2>
+     <div class="noct-preview">
+
+          <div class="noct-card" style="background-color: var(--noct-dark-primary); color: var(--noct-dark-onprimary);">
+               <h3 class="align-center">Primary</h3>
+          </div>
+          <div class="noct-card" style="background-color: var(--noct-dark-secondary); color: var(--noct-dark-onsecondary);">
+               <h3 class="align-center">Secondary</h3>
+          </div>
+          <div class="noct-card" style="background-color: var(--noct-dark-tertiary); color: var(--noct-dark-ontertiary);">
+               <h3 class="align-center">Tertiary</h3>
+          </div>
+          <div class="noct-card" style="background-color: var(--noct-dark-error); color: var(--noct-dark-onerror);">
+               <h3 class="align-center">Error</h3>
+          </div>
+          <div class="noct-card" style="background-color: var(--noct-dark-hover); color: var(--noct-dark-onhover);">
+               <h3 class="align-center">Hover</h3>
+          </div>
+          <div class="noct-card" style="background-color: var(--noct-dark-surfacevariant); color: var(--noct-dark-onsurfacevariant);">
+               <h3 class="align-center">Surface Variant</h3>
+          </div>
+
+     </div>
+</div>
+
+
+</template>
+
+<style scoped>
+
+.noct-preview {
+     width: 100%;
+     display: grid;
+     gap: 1rem;
+     grid-template-columns: 1fr 1fr 1fr;
+     grid-auto-flow: row;
+     grid-auto-rows: min-content;
+}
+
+.noct-card {
+     padding: 1rem;
+     height: 3rem;
+     border-radius: 1rem;
+     border: 1px solid var(--noct-dark-outline);
+     box-shadow: 0.25rem 0.25rem 0.5rem var(--noct-dark-shadow);
+}
+
+
+</style>
---
+++ src/components/previews/noctalia-light.vue
@@ -0,0 +1,61 @@
+<script setup lang="ts">
+import { usePaletteStore } from '@/stores/palette';
+
+
+const paletteStore = usePaletteStore();
+
+</script>
+
+<template>
+
+<div style="background-color: var(--noct-light-surface); color: var(--noct-light-onsurface);">
+
+     <h2 class="align-center">Surface</h2>
+     <div class="noct-preview">
+
+          <div class="noct-card" style="background-color: var(--noct-light-primary); color: var(--noct-light-onprimary);">
+               <h3 class="align-center">Primary</h3>
+          </div>
+          <div class="noct-card" style="background-color: var(--noct-light-secondary); color: var(--noct-light-onsecondary);">
+               <h3 class="align-center">Secondary</h3>
+          </div>
+          <div class="noct-card" style="background-color: var(--noct-light-tertiary); color: var(--noct-light-ontertiary);">
+               <h3 class="align-center">Tertiary</h3>
+          </div>
+          <div class="noct-card" style="background-color: var(--noct-light-error); color: var(--noct-light-onerror);">
+               <h3 class="align-center">Error</h3>
+          </div>
+          <div class="noct-card" style="background-color: var(--noct-light-hover); color: var(--noct-light-onhover);">
+               <h3 class="align-center">Hover</h3>
+          </div>
+          <div class="noct-card" style="background-color: var(--noct-light-surfacevariant); color: var(--noct-light-onsurfacevariant);">
+               <h3 class="align-center">Surface Variant</h3>
+          </div>
+
+     </div>
+</div>
+
+
+</template>
+
+<style scoped>
+
+.noct-preview {
+     width: 100%;
+     display: grid;
+     gap: 1rem;
+     grid-template-columns: 1fr 1fr 1fr;
+     grid-auto-flow: row;
+     grid-auto-rows: min-content;
+}
+
+.noct-card {
+     padding: 1rem;
+     height: 3rem;
+     border-radius: 1rem;
+     border: 1px solid var(--noct-light-outline);
+     box-shadow: 0.25rem 0.25rem 0.5rem var(--noct-light-shadow);
+}
+
+
+</style>
