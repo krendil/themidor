7b4cd3165ea865f0eae257641d9bb308cafbb469
David Osborne <david.osborne@protonmail.com>
2026-02-02 14:57:51 +1000

    Add buttons to add missing required/recommended tags to project from exporter screen

--- src/components/exporters/BaseExporter.vue
+++ src/components/exporters/BaseExporter.vue
@@ -20,21 +20,39 @@ const missingRequired = computed(() => props.required
     .filter( t => !paletteStore.getColourByTag(t) )
     .map( t => ({
         tag: t,
-        description: library.descriptions[t]
+        description: library.descriptions[t],
+        absent: !(t in paletteStore.palette.tags)
     }))
 );
+
+const hasAbsentRequired = computed(() => missingRequired.value.map(t => t.absent).reduce((a, b) => a || b));
 
 const missingRecommended = computed(() => props.recommended
     .filter(t => !paletteStore.getColourByTag(t))
     .map(t => ({
         tag: t,
-        description: library.descriptions[t]
+        description: library.descriptions[t],
+        absent: !(t in paletteStore.palette.tags)
     }))
 );
+
+const hasAbsentRecommended = computed(() => missingRecommended.value.map(t => t.absent).reduce((a, b) => a || b));
 
 function download() {
     const blob = new Blob([props.output], { type: props.mime ?? 'text/plain' });
     saveAs(blob, props.filename);
+}
+
+function addRequired() {
+    for(var tag of missingRequired.value) {
+        paletteStore.palette.tags[tag.tag] = null;
+    }
+}
+
+function addRecommended() {
+    for(var tag of missingRecommended.value) {
+        paletteStore.palette.tags[tag.tag] = null;
+    }
 }
 
 </script>
@@ -47,17 +65,25 @@ function download() {
                 Not all required tags are assigned. Exported theme may contain errors.
             </div>
             <div v-if="missingRequired.length > 0" class="tag-group colour-transition">
-                <div class="group-title"><span>Required tags:</span></div>
+                <div class="group-title">
+                    <span>Required tags:</span>
+                    <div v-if="hasAbsentRequired" class="group-button"><button @click="addRequired">Add to project</button></div>
+                </div>
                 <transition-group name="chips" tag="div" class="group-chips">
                     <div v-for="tag in missingRequired" :key="tag.tag" class="tag-chip monospace colour-transition"
+                    :class="{absent: tag.absent}"
                     :title="tag.description" draggable="true"
                     @dragstart="onDragTag($event, tag.tag)">#{{ tag.tag }}</div>
                 </transition-group>
             </div>
             <div v-if="missingRecommended.length > 0" class="tag-group colour-transition">
-                <div class="group-title"><span>Recommended tags:</span></div>
+                <div class="group-title">
+                    <span>Recommended tags:</span>
+                    <div v-if="hasAbsentRecommended" class="group-button"><button @click="addRecommended">Add to project</button></div>
+                </div>
                 <transition-group name="chips" tag="div" class="group-chips">
                     <div v-for="tag in missingRecommended" :key="tag.tag" class="tag-chip monospace colour-transition"
+                    :class="{absent: tag.absent}"
                     :title="tag.description" draggable="true"
                     @dragstart="onDragTag($event, tag.tag)">#{{ tag.tag }}</div>
                 </transition-group>
@@ -76,6 +102,13 @@ function download() {
 
 <style lang="css" scoped>
 
+.group-title {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    height: 1.5rem; /* Fix height so there is no adjustment when button appears/disappears */
+}
+
 .group-chips {
   padding-bottom: 0.2rem;
 }
@@ -89,11 +122,15 @@ function download() {
   border-radius: 2px;
   padding: 0.1em;
 
-  border-color: currentColor;
-
   margin: 0.1rem;
 
   cursor: grab;
+}
+
+.absent {
+    color: var(--theme-hifg);
+    border-color: var(--theme-hifg);
+    background-color: var(--theme-hibg);
 }
 
 .chips-move, /* apply transition to moving elements */
